b1743a9de2c72fc2f1552a4ae3172b8a718f6db5
Angel Santiago <lsantiago@miumg.edu.gt>
2026-03-14 14:20:28 -0600

    avance parcial

--- Problema1/src/main/java/main.java
+++ Problema1/src/main/java/main.java
@@ -1,9 +1,34 @@
 
 public class main {
+	
+		    public static int calcularPuntaje(int[] numeros) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		        int puntaje = 0;
 
-	}
+		        for (int n : numeros) {
+
+		            if (n == 5) {
+		                puntaje += 5;
+		            } 
+		            else if (n % 2 == 0) {
+		                puntaje += 1;   
+		            } 
+		            else {
+		                puntaje += 3;
+		            }
+
+		        }
+
+		        return puntaje;
+		    }
+
+		    public static void main(String[] args) {
+
+		        int[] numeros = {5,5,5};
+
+		        int resultado = calcularPuntaje(numeros);
+
+		        System.out.println("Puntaje total: " + resultado);
+		    }
 
 }
